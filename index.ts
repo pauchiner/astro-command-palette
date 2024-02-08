9b47de2b5e8f39baae9f35f12def0d7ee32b5341
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-08 18:57:46 +0100

    refactor(components): rename CommandPalette.astro to Component.astro

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import CommandPalette from './src/components/CommandPalette.astro';
+import CommandPalette from './src/components/Component.astro';
 export { default as CommandPaletteAction } from './src/components/Action.astro';
 
 export default CommandPalette;
