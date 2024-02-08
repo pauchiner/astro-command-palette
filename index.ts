759367ae31a442daf26c7465cb09fdbd4620a6e9
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-08 09:30:25 +0100

    feat(components): add the CommandPalette, CommandPaletteLinkAction and CommandPaletteButtonAction components

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-// @ts-expect-error
+// @ts-nocheck
 import CommandPalette from './src/components/CommandPalette.astro';
+export {default as CommandPaletteLinkAction} from './src/components/LinkAction.astro';
+export {default as CommandPaletteButtonAction} from './src/components/ButtonAction.astro';
 
 export default CommandPalette;
