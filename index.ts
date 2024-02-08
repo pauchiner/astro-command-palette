c9e5439dfa971d32be9295be97fb96a82c70c034
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-08 09:32:22 +0100

    fix(tsconfig): fix typo in tsconfig.json that avoids correct typing of the index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-// @ts-nocheck
 import CommandPalette from './src/components/CommandPalette.astro';
-export {default as CommandPaletteLinkAction} from './src/components/LinkAction.astro';
-export {default as CommandPaletteButtonAction} from './src/components/ButtonAction.astro';
+export { default as CommandPaletteLinkAction } from './src/components/LinkAction.astro';
+export { default as CommandPaletteButtonAction } from './src/components/ButtonAction.astro';
 
 export default CommandPalette;
