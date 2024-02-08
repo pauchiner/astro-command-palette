064002a65a453fe1ca31035abd711c0d43f02a7e
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-08 09:29:27 +0100

    feat: create a types file with all the interfaces of the plugin

---
+++ src/types.d.ts
@@ -0,0 +1,25 @@
+export interface CommandPalette {
+  placeholder?: string;
+  children: CommandPaletteItem[];
+}
+
+export interface CommandPaletteItem {
+  id: string;
+  children: string;
+}
+
+interface CommandPaletteAction extends CommandPaletteItem {
+  icon?: ChildNode;
+}
+
+interface CommandPaletteGroup extends CommandPaletteItem {
+  actions: Action[];
+}
+
+export interface CommandPaletteLinkAction extends CommandPaletteAction {
+  href: string;
+}
+
+export interface CommandPaletteButtonAction extends CommandPaletteAction {
+  onClick: () => void;
+}
