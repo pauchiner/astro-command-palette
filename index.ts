bf152590b45cb85cc58cae6c38cce0c7b0df1b49
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-09 21:51:26 +0100

    fix: update the index.ts exports

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import CommandPalette from './src/components/Component.astro';
-export { default as CommandPaletteAction } from './src/components/Action.astro';
+export {
+  default as createCommandPaletteItems,
+  openUrl
+} from './src/scripts/createItems';
 
 export default CommandPalette;
