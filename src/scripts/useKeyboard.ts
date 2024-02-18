a2a71a1267b1d4fd682509cefd8da6851c5a4584
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-18 20:08:57 +0100

    feat(scripts): create the useKeyboard hook

---
+++ src/scripts/useKeyboard.ts
@@ -0,0 +1,69 @@
+import {
+  incrementItem,
+  dispatchAction,
+  decrementItem,
+  getContainer
+} from './internals';
+
+const useKeys = (event: KeyboardEvent) => {
+  /*
+   * The main shortcut to open a close the command palette.
+   * @returns if the keybinding is pressed
+   */
+  const commandPressed = event.metaKey && event.key === 'k';
+
+  /*
+   * The shortcut to close the command palette or exit from a submenu.
+   * @returns if the keybinding is pressed
+   */
+  const escapePressed = event.key === 'Escape';
+
+  /*
+   * The shortcut to run an action inside the command palette.
+   * @returns if the keybinding is pressed
+   */
+  const enterPressed = event.key === 'Enter';
+
+  /*
+   * The shortcut to move down between actions inside the command palette.
+   * @returns if the keybinding is pressed
+   */
+  const downPressed =
+    event.key === 'ArrowDown' || (!event.shiftKey && event.key === 'Tab');
+
+  /*
+   * The shortcut to move up between actions inside the command palette.
+   * @returns if the keybinding is pressed
+   */
+  const upPressed =
+    event.key === 'ArrowUp' || (event.shiftKey && event.key === 'Tab');
+
+  return { commandPressed, escapePressed, enterPressed, downPressed, upPressed };
+};
+
+const handleKeyboard = (event: KeyboardEvent) => {
+  const { isVisible } = getContainer();
+  const { commandPressed, escapePressed, enterPressed, downPressed, upPressed } =
+    useKeys(event);
+
+  if (isVisible && (commandPressed || escapePressed)) closeCommandPalette();
+  if (!isVisible && commandPressed) openCommandPalette();
+
+  if (isVisible) {
+    if (downPressed) {
+      event.preventDefault();
+      incrementItem();
+    }
+    if (upPressed) {
+      event.preventDefault();
+      decrementItem();
+    }
+    if (enterPressed) {
+      event.preventDefault();
+      dispatchAction();
+    }
+  }
+};
+
+const { container } = getContainer();
+container.addEventListener('keydown', handleKeyboard);
