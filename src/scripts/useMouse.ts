517424b6ccce65d13f2cb5440b690f9b74f1615b
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-19 17:56:44 +0100

    feat(scripts): add the useMouse hook

---
+++ src/scripts/useMouse.ts
@@ -0,0 +1,27 @@
+import {setCurrentItem, getElements} from './internals';
+import {closeCommandPalette} from './useCommandPalette';
+
+const useMouse = () => {
+  const {actionItems, container} = getElements();
+
+  const handleHover = (event: MouseEvent): void => {
+    const isVisible = container.getAttribute('data-visible') === 'true';
+    if (!isVisible) return;
+
+    const target = event.target as HTMLElement;
+
+    actionItems.forEach(({id}, index) => {
+      if (target.id === id) setCurrentItem(index);
+    });
+  };
+
+  const handleClick = (event: MouseEvent): void => {
+    const target = event.target as HTMLElement;
+
+    if (target.id === 'command-palette-container') closeCommandPalette();
+  };
+
+  return {handleHover, handleClick};
+};
+
+export default useMouse;
