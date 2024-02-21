856ee3cd7fd508030ea21ccb4723aa922a505148
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-21 17:17:23 +0100

    fix(scripts): remove the handleHover function of the mouse

--- src/scripts/mouse.ts
+++ src/scripts/mouse.ts
@@ -1,17 +1,4 @@
-import {setCurrentItem, getElements} from './internals';
 import {closeCommandPalette} from './command-palette';
-
-export const handleHover = (event: MouseEvent): void => {
-  const {actionItems, container} = getElements();
-  const isVisible = container.getAttribute('data-visible') === 'true';
-  if (!isVisible) return;
-
-  const target = event.target as HTMLElement;
-
-  actionItems.forEach(({id}, index) => {
-    if (target.id === id) setCurrentItem(index);
-  });
-};
 
 export const handleClick = (event: MouseEvent): void => {
   const target = event.target as HTMLElement;
