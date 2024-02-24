f267b16d11f8650046d03c63cba4f648b011d2e7
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-24 16:11:11 +0100

    feat(scripts/internals): add navigation utilities to handle page routes

---
+++ src/scripts/internals/navigation.ts
@@ -0,0 +1,44 @@
+import store from './store';
+
+const STORAGE_KEY = '_currentRoute';
+
+const setRoute = (route: string) => {
+  store.setItem(STORAGE_KEY, route);
+};
+
+export const getCurrentRoute = () => {
+  try {
+    return store.getItem(STORAGE_KEY) ?? '';
+  } catch {
+    return '';
+  }
+};
+
+export const navigate = (route: string) => {
+  const current = getCurrentRoute();
+
+  if (current === '') {
+    setRoute(route);
+    return;
+  }
+
+  setRoute(`${current}/${route}`);
+};
+
+export const goBack = () => {
+  const current = getCurrentRoute();
+
+  if (current === '') {
+    return;
+  }
+
+  if (!current.includes('/')) {
+    setRoute('');
+    return;
+  }
+
+  const routes = current.split('/');
+  routes.pop();
+
+  setRoute(routes.join('/'));
+};
