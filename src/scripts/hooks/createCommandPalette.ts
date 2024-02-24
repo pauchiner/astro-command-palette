809ade431520c23ea067518a92f0dac2859c28cb
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-24 21:37:48 +0100

    fix(scripts/hooks): update createCommandPalette hook to use the new internal render

--- src/scripts/hooks/createCommandPalette.ts
+++ src/scripts/hooks/createCommandPalette.ts
@@ -1,36 +1,10 @@
-import type {CommandPaletteAction, CommandPaletteItem} from '../../types';
-import createActionItem from '../../components/Action';
+import type { CommandPaletteItem } from '../../types';
+import { renderItems } from '../internals/render';
+import store from '../internals/store';
 
-const dispatchActionItem = (item: CommandPaletteAction) => {
-  const action = document.querySelector(`#${item.id}`) as HTMLElement;
-  if (item.handler) action.addEventListener('click', item.handler);
-  if (item.url)
-    action.addEventListener('click', () =>
-      window.open(item.url, '_blank', 'noopener nofollow')
-    );
-};
-
-const createUID = () => {
-  return (
-    Date.now().toString(36) +
-    Math.floor(
-      Math.pow(10, 12) + Math.random() * 9 * Math.pow(10, 12)
-    ).toString(36)
-  );
-};
-
-const createCommandPaletteItems = (items: CommandPaletteItemProps[]) => {
-  const listToAttach = document.querySelector(
-    'command-palette-items'
-  ) as HTMLDivElement;
-  listToAttach.innerText = '';
-
-  items.forEach(data => {
-    const item = { ...data, id: createUID() };
-    const component = createCommandPaletteItem(item);
-    listToAttach.append(component);
-    dispatchActionItem(item);
-  });
+const createCommandPaletteItems = (items: CommandPaletteItem[]) => {
+  store.setItem('items', items);
+  renderItems();
 };
 
 export default createCommandPaletteItems;
