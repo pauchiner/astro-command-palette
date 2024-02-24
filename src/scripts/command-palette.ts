448cf43cba0af68d44ad34dfe678fd93daf272c3
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-24 22:49:54 +0100

    fix: update the tsdoc comments

--- src/scripts/command-palette.ts
+++ src/scripts/command-palette.ts
@@ -2,8 +2,8 @@ import {navigate} from 'astro/virtual-modules/transitions-router.js';
 import {setCurrentItem, getElements, dispatchSearch} from './internals';
 
 /**
- * Opens the command palette by displaying it and setting the current item to the first item.
- * Also focuses on the command palette input.
+ * Opens the command palette by displaying it, setting the current item to the
+ * first item, and focusing on the command palette input.
  * @returns void
  */
 export const openCommandPalette = (): void => {
@@ -20,7 +20,8 @@ export const openCommandPalette = (): void => {
 };
 
 /**
- * Closes the command palette by hiding it and clearing the input value after a short delay.
+ * Closes the command palette by hiding it, clearing the input value, and
+ * dispatching a search after a short delay.
  * @returns void
  */
 export const closeCommandPalette = (): void => {
@@ -32,6 +33,11 @@ export const closeCommandPalette = (): void => {
   navigate('');
 };
 
+/**
+ * Hides the command palette without clearing the input value or
+ * dispatching a search.
+ * @returns void
+ */
 export const hideCommandPalette = (): void => {
   const {container} = getElements();
   setTimeout(() => container.setAttribute('data-visible', 'false'), 150);
