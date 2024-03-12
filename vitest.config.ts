9c59cf06aa9068d388993baa15325fdda7fbaf7a
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-03-12 12:54:59 +0100

    test: exclude example from test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     coverage: {
-      provider: 'istanbul'
+      provider: 'istanbul',
+      exclude: ['**/example/**']
     }
   }
 });
